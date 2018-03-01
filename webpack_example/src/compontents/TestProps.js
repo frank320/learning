72598057e21630a87b2383e0bc1575c4dac7a152
frank@tuxing <zhangshipeng@tuxing2010.com>
2018-03-01 11:01:22 +0800

    test life

--- webpack_example/src/compontents/TestProps.js
+++ webpack_example/src/compontents/TestProps.js
@@ -9,16 +9,39 @@ class Child extends Component {
     }
   }
 
-  componentWillReceiveProps (nextProps) {
+  needUpdate = true //定义实例属性
+
+  componentWillReceiveProps (nextProps, nextState) {
+    //在重新渲染之前 改变子组件的state 但不会引起第二次渲染
     const text = nextProps.text
+    //onsole.log(nextProps)
+    this.needUpdate = !(text === this.state.text) //判断是否需要重新渲染组件
+    //console.log(this.needUpdate)
     this.setState({
       text
     })
     //console.log(this.state)
+    console.log('子组件componentWillReceiveProps')
+  }
+
+  shouldComponentUpdate () {
+    //console.log(this.state)
+    console.log('子组件shouldComponentUpdate')
+    return this.needUpdate
+  }
+
+  // 组件将要被重新渲染
+  componentWillUpdate () {
+    console.log('子组件conpontentWillUpdate')
+  }
+
+  // 组件已经被重新渲染
+  componentDidUpdate () {
+    console.log('子组件conpontentDidUpdate')
   }
 
   render () {
-    console.log(111)
+    console.log('子组件渲染')
     return <p>{this.state.text}</p>
   }
 }
@@ -28,11 +51,26 @@ class Parent extends Component {
     name: 'xxx'
   }
 
+  componentWillReceiveProps (nextProps, nextState) {//该周期函数不会执行
+    console.log('父组件componentWillReceiveProps')
+  }
+  // 组件将要被重新渲染
+  componentWillUpdate () {
+    console.log('父组件conpontentWillUpdate')
+  }
+
+  // 组件已经被重新渲染
+  componentDidUpdate () {
+    console.log('父组件conpontentDidUpdate')
+  }
+
   render () {
+    console.log('父组件渲染')
     return (
       <div>
         <Child text={this.state.name}/>
-        <button onClick={() => this.setState({name: 'zzz'})}>change</button>
+        <Child text={this.state.name}/>
+        <button onClick={() => this.setState({name: 'zzz'})}>chang texte</button>
       </div>
     )
   }
