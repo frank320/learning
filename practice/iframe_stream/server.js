fae0ad28ef7767abc182362304c9c27c2e57c6d2
zhangshipeng <zhangshipeng@douyu.tv>
2019-04-04 16:35:39 +0800

    immer学习

---
+++ practice/iframe_stream/server.js
@@ -0,0 +1,15 @@
+//iframe流
+let express = require('express')
+let app = express()
+app.use(express.static(__dirname))
+app.get('/clock', function(req, res) {
+  setInterval(function() {
+    let date = new Date().toLocaleString()
+    res.write(`
+       <script type="text/javascript">
+         parent.document.getElementById('clock').innerHTML = "${date}";//改变父窗口dom元素
+       </script>
+     `)
+  }, 1000)
+})
+app.listen(8080)
