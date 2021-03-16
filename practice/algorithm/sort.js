7f590cf6deaa46f1bf4d8529eb7983fd7944c348
张世鹏 <zhangshipeng@douyu.tv>
2021-03-16 15:32:42 +0800

    添加二分查找算法

--- practice/algorithm/sort.js
+++ practice/algorithm/sort.js
@@ -1,5 +1,12 @@
 const arr = [3, 7, 6, 8, 4, 2, 1, 5]
 
+let a = 1;
+let b = 2;
+
+[b, a] = [a, b]
+console.log(a,b)
+
+
 function swap(a, b) {
     [arr[a], arr[b]] = [arr[b], arr[a]]
 }
@@ -86,3 +93,24 @@ function mergeSort(arr) {
 }
 
 console.log(mergeSort(arr))
+
+//二分查找
+function binary_search(arr, key) {
+    var low = 0,
+        high = arr.length - 1;
+    while(low <= high){
+        var mid = parseInt((high + low) / 2);
+        if(key == arr[mid]){
+            return  mid;
+        }else if(key > arr[mid]){
+            low = mid + 1;
+        }else if(key < arr[mid]){
+            high = mid -1;
+        }else{
+            return -1;
+        }
+    }
+};
+var arr = [1,2,3,4,5,6,7,8,9,10,11,23,44,86];
+var result = binary_search(arr,10);
+alert(result); // 9 返回目标元素的索引值       
