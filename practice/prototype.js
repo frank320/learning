a51ab0049f1f2afdcf8ba30af2e060811edb6e85
张世鹏 <zhangshipeng@douyu.tv>
2021-07-20 11:05:46 +0800

    add prototype test

--- practice/prototype.js
+++ practice/prototype.js
@@ -9,4 +9,21 @@ Cat.prototype.sayHello = function () {
 
 const cat = new Cat()
 
-cat.sayHello()
+cat.sayHello() //hello hello
+
+
+function Animal() {
+  this.name = 'animal';
+}
+Animal.prototype.sayName = function() {
+  alert(this.name);
+};
+
+function Person() {
+  Animal.call(this); // apply, call, bind方法都可以．细微区别，后面会提到．
+}
+
+const p = new Person()
+
+console.log(p.name)//animal
+console.log(p.sayName)//undefined
